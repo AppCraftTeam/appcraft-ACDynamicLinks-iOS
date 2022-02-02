aef63e79f3325f30b9747979c15cc8900c31f5ae
Anatoly Rudenko <anatoliy.rudenko@appcraft.team>
2022-02-02 16:16:58 +0700

    switch to protocols

---
+++ Sources/ACDynamicLink/Interface/Generator.swift
@@ -0,0 +1,15 @@
+//
+//  Generator.swift
+//  
+//
+//  Created by Анатолий Руденко on 02.02.2022.
+//
+
+import Foundation
+
+protocol Generator {
+    
+    var data: GenerationData { get }
+    
+    func generate(path: Navigatable, isShort: Bool, completion: @escaping (URL?) -> Void)
+}
