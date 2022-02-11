8125bd32b7ec184b8e33e7457c1556d82e302348
Anatoly Rudenko <anatoliy.rudenko@appcraft.team>
2022-02-11 17:11:50 +0700

    rename domainURLPrefix

--- Sources/ACDynamicLink/Interface/GenerationData.swift
+++ Sources/ACDynamicLink/Interface/GenerationData.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public protocol GenerationDataProvider {
     var urlString: String { get }
-    var domainURIPrefix: String { get }
+    var domainURLPrefix: String { get }
     var iOSBundle: String { get }
     
     // optionals
@@ -20,14 +20,14 @@ public protocol GenerationDataProvider {
 public struct GenerationData: GenerationDataProvider {
     
     public let urlString: String
-    public let domainURIPrefix: String
+    public let domainURLPrefix: String
     public let iOSBundle: String
     public let appStoreID: String?
     public let androidBundle: String?
     
-    public init(urlString: String, domainURIPrefix: String, iOSBundle: String, appStoreID: String?, androidBundle: String?) {
+    public init(urlString: String, domainURLPrefix: String, iOSBundle: String, appStoreID: String?, androidBundle: String?) {
         self.urlString = urlString
-        self.domainURIPrefix = domainURIPrefix
+        self.domainURLPrefix = domainURLPrefix
         self.iOSBundle = iOSBundle
         self.appStoreID = appStoreID
         self.androidBundle = androidBundle
