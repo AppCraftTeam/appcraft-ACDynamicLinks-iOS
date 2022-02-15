8a0284722f7b4f1e9685b12746085a772a2f236b
Anatoly Rudenko <anatoliy.rudenko@appcraft.team>
2022-02-15 17:10:38 +0700

    min iOS target = 11

--- Package.swift
+++ Package.swift
@@ -6,10 +6,9 @@ import PackageDescription
 let package = Package(
     name: "ACDynamicLink",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v11)
         ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ACDynamicLink",
             targets: ["ACDynamicLink"]),
@@ -18,8 +17,6 @@ let package = Package(
         .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "8.0.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ACDynamicLink",
             dependencies: [
